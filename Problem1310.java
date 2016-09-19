8eb86b5719cba581f5f8d650740dd7f4a2ab4069
Jeferson Barros <im.jbalves@gmail.com>
2016-09-19 18:29:57 -0400

    Update Problem1310.java

--- Problem1310.java
+++ Problem1310.java
@@ -10,28 +10,28 @@ public class Problem1310 {
         BufferedReader in = new BufferedReader(ir);
 		
         int days;
-		int perDayCost;
-		int profit =0;
+	int perDayCost;
+	int profit =0;
 		
-		days = Integer.parseInt(in.readLine());
-		perDayCost = Integer.parseInt(in.readLine());
+	days = Integer.parseInt(in.readLine());
+	perDayCost = Integer.parseInt(in.readLine());
 		
-		int[] array = new int[days];
-		for (int i = 0; i < days; i++) {
-			array[i] = Integer.parseInt(in.readLine());
-		}
-	    
-		for (int subArraySize = 1; subArraySize <= days; ++subArraySize) {
-			
-			for (int startIndex = 0; startIndex < days; ++startIndex) {
-				if (startIndex+subArraySize > days)
-					break;
-				int max = 0;
-				for (int i = startIndex; i < (startIndex+subArraySize); i++)
-					max+=array[i]-perDayCost;
+	int[] array = new int[days];
+	for (int i = 0; i < days; i++) {
+		array[i] = Integer.parseInt(in.readLine());
+	}
+	   
+	for (int subArraySize = 1; subArraySize <= days; ++subArraySize) {
+		
+		for (int startIndex = 0; startIndex < days; ++startIndex) {
+			if (startIndex+subArraySize > days)
+				break;
+			int max = 0;
+			for (int i = startIndex; i < (startIndex+subArraySize); i++)
+				max+=array[i]-perDayCost;
 				
-				if (max>profit)
-					profit = max;
+			if (max>profit)
+				profit = max;
 			}
 			
 		}
